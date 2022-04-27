459e1e694faccf1b409dd308df4245bb8ede8e21
Haejeong Cho <s2036@e-mirim.hs.kr>
2022-04-27 09:47:36 +0900

    #1 윈폼 크기, 이름 변경

--- MyHelloCS/Form1.Designer.cs
+++ MyHelloCS/Form1.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "Form1";
+            this.UseWaitCursor = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- MyHelloCS/Form1.cs
+++ MyHelloCS/Form1.cs
@@ -16,5 +16,10 @@ namespace MyHelloCS
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
